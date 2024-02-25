cc6d1b9231e47e9aa5eb8712c77167c39461321a
Francisco Felipe <46639197+felmp@users.noreply.github.com>
2024-02-25 15:37:41 -0300

    feat: first configuration gamem

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${inter.className} antialiased bg-slate-200`}>
+        {children}
+      </body>
     </html>
   )
 }
