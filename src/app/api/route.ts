cf6b0d8dc17428596d834a4f17e4044ef434a7e1
Lindson França <44710146+L1NDS0N@users.noreply.github.com>
2024-02-25 17:49:14 +0000

    feat: prisma added, first db insert

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,10 +1,15 @@
 import { prisma } from "./core/repositories/prisma";
 
 export async function GET() {
-  const word = await prisma.word.create({
-    data: {
-      name: "Teste",
-    },
+  const word = await prisma.$transaction(async (prisma) => {
+    const createdWord = await prisma.word.create({
+      data: {
+        name: "Teste",
+      },
+    });
+    return createdWord;
   });
   return Response.json(word);
-}
+  }
+  
+  
