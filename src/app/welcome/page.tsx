56f500c759c8b4e59a5af2af641952fb0278d112
Lindson França <44710146+L1NDS0N@users.noreply.github.com>
2024-03-09 10:15:09 -0300

    refact: component button

--- src/app/welcome/page.tsx
+++ src/app/welcome/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import XButton from "@/components/XButton";
 import { useRouter } from "next/navigation";
 import guessWhatImg from "../../assets/guess-what.png";
 export default function WelcomePage() {
@@ -16,12 +17,12 @@ export default function WelcomePage() {
       <p className="mb-6 text-lg">
         Tente adivinhar a palavra oculta e divirta-se.
       </p>
-      <button
+      <XButton
         onClick={() => router.push("/new-game")}
         className="w-full max-w-md rounded-full bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-400"
       >
         Novo Jogo
-      </button>
+      </XButton>
     </>
   );
 }
