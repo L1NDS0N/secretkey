a5156e2720ce816011d5a14edde3f844ba7d3a74
Lindson França <44710146+L1NDS0N@users.noreply.github.com>
2024-03-18 13:57:45 -0300

    feat: cronometer

--- src/app/play/[slug]/page.tsx
+++ src/app/play/[slug]/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import XStepper from "@/components/XStepper";
+import { useEffect, useState } from "react";
 
 type PreviousGames = {
   slug: string;
@@ -11,6 +12,9 @@ export default function PlayPage({ params }: { params: { slug: string } }) {
   const { slug } = params;
   const gameStorageKey = "sk@game_config";
   const alreadyInRoom = localStorage.getItem(gameStorageKey);
+  const maxSeconds = 210;
+  const [cronometerSeconds, setCronometerSeconds] = useState(maxSeconds);
+
   if (!alreadyInRoom) {
     localStorage.setItem(
       gameStorageKey,
@@ -37,11 +41,22 @@ export default function PlayPage({ params }: { params: { slug: string } }) {
     { name: "João" },
     { name: "Gisele" },
   ];
+
+  useEffect(() => {
+    const unsubscribe = setTimeout(() => {
+      if (cronometerSeconds > 0) {
+        setCronometerSeconds(cronometerSeconds - 1);
+      }
+    }, 1000);
+
+    return () => clearTimeout(unsubscribe);
+  }, [cronometerSeconds]);
+
   return (
     <section className=" flex min-h-96 flex-1 justify-center">
       <aside className="flex min-w-24 max-w-24 flex-col items-center justify-center gap-4">
-        {players.map((player) => (
-          <div className="flex h-full w-full">
+        {players.map((player, id) => (
+          <div className="flex h-full w-full" key={id}>
             <div className="flex w-full flex-1 cursor-pointer items-center justify-center rounded bg-zinc-100 shadow  hover:bg-zinc-200">
               <span>{player.name}</span>
             </div>
@@ -57,12 +72,18 @@ export default function PlayPage({ params }: { params: { slug: string } }) {
           <h1>Palavra Misteriosa </h1>
         </div>
         <div className="flex  flex-1 items-end justify-center">
-          <XStepper min={0} max={210} step={1} />
+          <XStepper
+            min={0}
+            max={maxSeconds}
+            step={1}
+            value={cronometerSeconds}
+            onChange={(e) => setCronometerSeconds(Number(e.target.value))}
+          />
         </div>
       </section>
       <aside className="flex min-w-24 max-w-24  flex-col items-center justify-center gap-4">
-        {players.map((player) => (
-          <div className="flex h-full w-full">
+        {players.map((player, id) => (
+          <div className="flex h-full w-full" key={id}>
             <div className="flex flex-col">
               <small>1x</small>
             </div>
--- src/components/XStepper.tsx
+++ src/components/XStepper.tsx
@@ -1,34 +1,29 @@
 "use client";
-import React, { useState } from "react";
-interface StepperProps {
+
+import React, { InputHTMLAttributes } from "react";
+import "./styles/XStepper.css";
+
+type XStepper = InputHTMLAttributes<HTMLInputElement>;
+interface StepperProps extends XStepper {
   min: number;
   max: number;
   step: number;
 }
 
-import "./styles/XStepper.css";
-
-const Stepper: React.FC<StepperProps> = ({ min, max, step }) => {
-  const [value, setValue] = useState(min);
-
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setValue(parseInt(event.target.value));
-  };
-
+const Stepper: React.FC<StepperProps> = ({ min, max, step, ...rest }) => {
   return (
     <div className="flex w-full items-center space-x-4">
       <div>
         <input
+          {...rest}
           type="range"
           min={min}
           max={max}
           step={step}
-          value={value}
-          onChange={handleChange}
           className="stepper-input h-2 w-full appearance-none rounded-lg bg-emerald-300 text-orange-200 outline-none"
         />
       </div>
-      <span className="text-xl font-semibold">{value}</span>
+      <span className="text-xl font-semibold">{rest.value}</span>
     </div>
   );
 };
